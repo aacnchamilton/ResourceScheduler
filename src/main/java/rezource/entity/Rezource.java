9e28587af28b5c55967f0146eddde2273e8197c8
aacnchamilton@gmail.com <aacnchamilton@gmail.com>
2023-03-07 17:07:45 -0700

    backup

--- src/main/java/rezource/entity/Rezource.java
+++ src/main/java/rezource/entity/Rezource.java
@@ -1,15 +1,7 @@
 package rezource.entity;
 
 import java.time.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Rezource {
   private long rezourceId;
   private String rezourceName;
